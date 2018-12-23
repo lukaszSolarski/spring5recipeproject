d3972650fcb0d65320e0c707bd11f86475e1d70d
Lukasz.Solarski <lukasz.solar@gmail.com>
2018-12-23 19:29:22 +0100

    Initial commit

---
+++ src/main/java/com/solar/guru/spring5recipeproject/Spring5recipeprojectApplication.java
@@ -0,0 +1,14 @@
+package com.solar.guru.spring5recipeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5recipeprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5recipeprojectApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/solar/guru/spring5recipeproject/Spring5recipeprojectApplicationTests.java
@@ -0,0 +1,17 @@
+package com.solar.guru.spring5recipeproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5recipeprojectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
