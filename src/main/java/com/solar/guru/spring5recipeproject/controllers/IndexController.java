a03fb2bc4b09acfafa72d15a9d88217f5afad9ec
Lukasz.Solarski <lukasz.solar@gmail.com>
2019-01-18 23:41:24 +0100

    First IndexController unit test

--- src/main/java/com/solar/guru/spring5recipeproject/controllers/IndexController.java
+++ src/main/java/com/solar/guru/spring5recipeproject/controllers/IndexController.java
@@ -15,28 +15,15 @@ import java.util.Optional;
 @Controller
 @Slf4j
 public class IndexController {
-    private final CategoryRepository categoryRepository;
-    private final UnitOfMeasureRepository unitOfMeasureRepository;
     private final RecipeService recipeService;
 
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository, RecipeService recipeService) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    public IndexController(RecipeService recipeService) {
         this.recipeService = recipeService;
     }
 
     @RequestMapping({"","/","/index","/index.html"})
     public String getIndex(Model model) {
         log.info("Performing getIndex method in " + this.getClass().getName());
-        Optional<Category> categoryOptional = categoryRepository.findByName("Polish");
-        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("teaspoon");
-        Optional<Category> notExistingCategoryOptional = categoryRepository.findByName("Polish2");
-        System.out.println("Category: " + categoryOptional.get().getId() + ":" + categoryOptional.get().getName());
-        System.out.println("Unit: " + unitOfMeasureOptional.get().getId() + ":" + unitOfMeasureOptional.get().getDescription());
-        /*the above line will throw 'java.util.NoSuchElementException: No value present' and the index page will return
-        HTTP 500 error
-         */
-        //notExistingCategoryOptional.get();
         model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
---
+++ src/test/java/com/solar/guru/spring5recipeproject/controllers/IndexControllerTest.java
@@ -0,0 +1,37 @@
+package com.solar.guru.spring5recipeproject.controllers;
+
+import com.solar.guru.spring5recipeproject.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class IndexControllerTest {
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    IndexController indexController;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    public void getIndex() {
+        assertEquals("index", indexController.getIndex(model));
+        verify(recipeService,times(1)).getRecipes();
+        verify(model,times(1)).addAttribute(eq("recipes"), anySet());
+    }
+}
