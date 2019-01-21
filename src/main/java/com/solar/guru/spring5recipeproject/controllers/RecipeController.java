7a5b180ae5315d54200addbecdcd4f8617f0cae0
Lukasz.Solarski <lukasz.solar@gmail.com>
2019-01-22 00:47:43 +0100

    Add possibility of creating new recipe (by url: /recipe/new)

--- src/main/java/com/solar/guru/spring5recipeproject/controllers/RecipeController.java
+++ src/main/java/com/solar/guru/spring5recipeproject/controllers/RecipeController.java
@@ -1,9 +1,12 @@
 package com.solar.guru.spring5recipeproject.controllers;
 
+import com.solar.guru.spring5recipeproject.commands.RecipeCommand;
 import com.solar.guru.spring5recipeproject.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,5 +21,20 @@ public class RecipeController {
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+        //TODO create tests
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedRecipe = recipeService.saveRecipeCommand(command);
+        return "redirect:/recipe/show/" + savedRecipe.getId();
+        //TODO create tests
     }
 }
